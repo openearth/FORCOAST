7da60eea7b89b8af1a0d36a5e8b95e2373058c93
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2021-02-16 10:11:57 +0100

    Add files via upload
    Added under Bivalve Mariculture, Portugues data via their thredds server and via local data, although none of them work yet.

--- src/lib/build-wms-layer.js
+++ src/lib/build-wms-layer.js
@@ -1,18 +1,20 @@
 import buildGeoserverUrl from './build-geoserver-url';
 import wms from './wms';
 
-export default ({ url, id, layer, style='', paint={} }) => {
+export default ({ url, id, layer, styles='', paint={} }) => {
   const tile = buildGeoserverUrl({
     url,
     service: 'WMS',
     request: 'GetMap',
     layers: layer,
-    style,
-    width: 256,
-    height: 256,
-    srs: 'EPSG:3857',
+    styles,
+    width: 2048,
+    height: 2048,
+    srs: 'EPSG:4326',
+    version: '1.3.0',
+    crs: 'CRS:84',
     transparent: true,
-    bbox: '{bbox-epsg-3857}',
+    bbox: '-9.648599624633789,38.161399841308594,-8.657400131225586,38.956600189208984',
     format: 'image/png',
     encode: false
   });
