3c8a7815241b335834b044403e963e6f410e75d6
IoannaMi <ioannamicha@hotmail.com>
2021-04-16 21:33:48 +0200

    add services view

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="pa-4 home d-flex flex-column">
+  <div v-if="selectedService" class="pa-4 home d-flex flex-column">
     <div>
-      <h2 class="h2">Services</h2>
-      <v-divider class="mt-4 mb-4" />
+      <h2 class="h2">{{ selectedCategory }}</h2>
+      <!-- <v-divider class="mt-4 mb-4" /> -->
     </div>
     <div>
-      <h2 class="h2">Placeholder for service stuff</h2>
-      
+      <service-viewer
+        :service="selectedService"
+      ></service-viewer>
+    </div>
+  </div>
+  <div v-else class="pa-4 home d-flex flex-column" >
+    <div>
+      <h2 class="h2">{{ selectedCategory }}</h2>
+      <v-divider class="mt-4 mb-4" />
+      <p>Select a service from the dropdown list </p>
     </div>
   </div>
 </template>
+<script>
+import { mapState } from 'vuex';
+import serviceViewer from '../components/service-viewer/service-viewer.vue';
+
+export default {
+  components: { serviceViewer },
+  computed: {
+    ...mapState({
+      selectedCategory: state => state.selectedCategory,
+      selectedService: state => state.selectedService
+
+    })
+  },
+}
+
+</script>
