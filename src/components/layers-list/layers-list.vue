94f57680790cc6799d06f2a81c2fd46d498513ed
IoannaMi <ioannamicha@hotmail.com>
2021-04-16 21:29:35 +0200

    add layers-list component
    contains data for sample structure of layers

---
+++ src/components/layers-list/layers-list.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-list class="pa-2" flat >
+      <v-list-item-group>
+        <template
+          v-for="(layer, index) in layers"
+        >
+          <v-list-item 
+            :key="layer.id" 
+          >  
+            <v-list-item-action>
+              <v-switch></v-switch>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ layer.name }}
+              </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-avatar>
+              <img :src="require(`@/assets/${layer.icon}`)">
+            </v-list-item-avatar>
+          </v-list-item>
+          <v-divider :key="index" v-if="index !== layers.length - 1"/>
+        </template>
+      </v-list-item-group>
+    </v-list> 
+  </div>  
+</template>
+
+<script>
+export default {
+    data() { 
+      return {
+        layers:[
+          {
+          id: "chw2-vector:gar_economy",
+          name: "Total capital stock",
+          layer: "chw2-vector:gar_economy",
+          url: "https://coastalhazardwheel.avi.deltares.nl/geoserver/ows",
+          icon:"fish.png"
+        },
+        {
+          id: "chw2-vector:test_layer",
+          name: "Total capital stock",
+          layer: "chw2-vector:gar_economy",
+          url: "https://coastalhazardwheel.avi.deltares.nl/geoserver/ows",
+          icon:"fish.png"
+        }
+          ],
+        icon:"@/assets/fish.png"
+      }
+       
+    }
+}
+ </script>
