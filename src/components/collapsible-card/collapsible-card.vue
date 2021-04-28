42aece9832d5b9c969647213a941210c9772e324
IoannaMi <ioannamicha@hotmail.com>
2021-04-28 20:03:05 +0200

    add props for disable; open

--- src/components/collapsible-card/collapsible-card.vue
+++ src/components/collapsible-card/collapsible-card.vue
@@ -1,32 +1,73 @@
 <template>
-  <v-expansion-panels v-model="open" class="mb-4">
+  <v-expansion-panels v-model="open" :disabled="disabled" class="mb-4">
     <v-expansion-panel>
-      <v-expansion-panel-header 
+      <v-expansion-panel-header
         color="primary"
         hide-actions
-        class="font-weight-bold px-4">
-        {{ title }}</v-expansion-panel-header>
-        <v-divider></v-divider>
+        class="font-weight-bold px-4"
+      >
+        {{ title }}
+        <template v-if="checked" v-slot:actions>
+          <v-icon>mdi-check-circle</v-icon>
+        </template>
+      </v-expansion-panel-header>
+      <v-divider></v-divider>
       <v-expansion-panel-content class="mx-n6">
         <slot />
+      </v-expansion-panel-content>
+      <v-expansion-panel-content v-if="nextButton">
+        <v-btn
+          block
+          color="primary"
+          :disabled="disabledButton"
+          elevation="2"
+          raised
+          >NEXT</v-btn
+        >
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   props: {
     title: {
       type: String,
       required: false,
     },
+    disabled: {
+      type: Boolean,
+    },
+    nextButton: {
+      type: Boolean,
+      required: false,
+    },
+    expand: {
+      type: Number,
+    },
   },
-  data() { 
-      return { 
-        open:0,
+  data() {
+    return {
+      open: this.expand,
+      checked: false,
+      disabledButton: true,
+    };
+  },
+  computed: {
+    ...mapState({
+      polygon: (state) => state.polygon,
+    }),
+  },
+  watch: {
+    polygon() {
+      if (this.polygon) {
+        this.disabledButton = false;
       }
-  }
+    },
+  },
 };
 /* 
 <div id="app">
--- src/components/collapsible-card/index.js
+++ src/components/collapsible-card/index.js
@@ -1,1 +1,1 @@
-export { default } from "./collapsible-card.vue";
+export { default } from "./collapsible-card.vue"
