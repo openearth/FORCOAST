a07aa048ce61c982ab49f3394c21e572991b72a1
IoannaMi <ioannamicha@hotmail.com>
2021-04-28 20:04:34 +0200

    format and clean; add selectable list; draggable marker; drawpolygon;

--- src/components/service-viewer/service-viewer.vue
+++ src/components/service-viewer/service-viewer.vue
@@ -3,36 +3,67 @@
     <div>
       <!-- <h2 class="h2">TEST</h2> -->
       <p>{{ service.name }}</p>
-      <p> {{ service.description }}</p>
+      <p>{{ service.description }}</p>
       <v-divider class="mt-4 mb-4" />
     </div>
     <!-- .sync -->
-    <collapsible-card v-if="service.layers"
-      title="Select a layer" 
+    <draggable-marker
+      v-if="service.components.draggable_marker"
+      @show-draggable-marker="onShowDraggableMarker"
+    ></draggable-marker>
+    <collapsible-card
+      v-if="service.components.draw_polygon"
+      :title="service.components.draw_polygon.title"
+      :polygon="polygon"
+      :nextButton="true"
+      :expand="1"
     >
-     <layers-list
-      :layers="service.layers"
-      @active-layers-change="onActiveLayerChange"
-      @active-legend-change="onActiveLegendChange"
-     ></layers-list>
+      <draw-polygon
+        :text="service.components.draw_polygon.text"
+        @show-draw-polygon="onShowDrawPolygon"
+      ></draw-polygon>
     </collapsible-card>
-    <collapsible-card v-if="service.date_span"
-      title="Select a start and an end date" 
-    >
-     <date-span></date-span>
-    </collapsible-card>
-    <collapsible-card v-if="service.date"
-      title="Select a date" 
-    >
-     <single-date></single-date>
-    </collapsible-card>
-    <div v-if="service.execute" class="align-self-center position-fixed"> <!-- TODO: Fixed position of button I prefer at the button -->
-      <v-btn 
-        color = "primary"
-        dark
-        class="ml-auto" 
-        >{{ service.execute }}</v-btn
+    <div v-if="service.components.selectable_lists">
+      <collapsible-card
+        v-for="list in service.components.selectable_lists"
+        :key="list"
+        :title="list.title"
+        :nextButton="true"
+        :disabled="true"
+        :expand="1"
       >
+        <selectable-list :values="list.values"></selectable-list>
+      </collapsible-card>
+    </div>
+    <collapsible-card
+      v-if="service.components.layers"
+      :expand="0"
+      title="Select a layer"
+    >
+      <layers-list
+        :layers="service.components.layers"
+        @active-layers-change="onActiveLayerChange"
+        @active-legend-change="onActiveLegendChange"
+      ></layers-list>
+    </collapsible-card>
+    <collapsible-card
+      v-if="service.components.date_span"
+      title="Select a start and an end date"
+      :expand="0"
+    >
+      <date-span></date-span>
+    </collapsible-card>
+    <collapsible-card
+      v-if="service.components.date"
+      :expand="0"
+      title="Select a date"
+    >
+      <single-date></single-date>
+    </collapsible-card>
+    <div v-if="service.execute_action" class="align-self-center position-fixed">
+      <v-btn color="primary" dark class="ml-auto">{{
+        service.execute_action
+      }}</v-btn>
     </div>
   </div>
 </template>
@@ -41,48 +72,50 @@ import CollapsibleCard from "@/components/collapsible-card";
 import LayersList from "@/components/layers-list";
 import SingleDate from "@/components/single-date";
 import DateSpan from "@/components/date-span";
+import SelectableList from "@/components/selectable-list";
+import DraggableMarker from "@/components/draggable-marker";
+import DrawPolygon from "@/components/draw-polygon";
 
-
-export default { 
-    components: { 
-        CollapsibleCard,
-        LayersList,
-        SingleDate,
-        DateSpan,
+export default {
+  components: {
+    CollapsibleCard,
+    LayersList,
+    SingleDate,
+    DateSpan,
+    DraggableMarker,
+    DrawPolygon,
+    SelectableList,
+  },
+  props: {
+    service: {
+      type: Object,
+      required: true,
     },
-    props: {
-      service: {
-        type:Object,
-        required: true,
-      }
+  },
+  data() {
+    return {
+      title: "Select a layer",
+      layersOn: [],
+    };
+  },
+  methods: {
+    onActiveLayerChange(activelayers) {
+      this.$emit("active-layers-change", activelayers);
     },
-    data() { 
-        return { 
-            title: "Select a layer",
-            layersOn: [],
-        }
+    onActiveLegendChange(legend) {
+      this.$emit("active-legend-change", legend);
     },
-    methods: {
-      onActiveLayerChange(activelayers) { 
-        this.$emit("active-layers-change", activelayers)
-      },
-      onActiveLegendChange(legend) {
-        this.$emit("active-legend-change", legend)
-      }
+    onShowDraggableMarker(event) {
+      this.$emit("show-draggable-marker", event);
     },
-    mounted() {
-      console.log('service-viewer prop service passed', this.service)
-    }
-/*     computed: {
-      ...mapState({
-        selectedService: state => state.selectedService
-      }),
-    }, */
-
-}
+    onShowDrawPolygon(event) {
+      this.$emit("show-draw-polygon", event);
+    },
+  },
+};
 </script>
 <style>
 .execute {
-  position:fixed;
-  }
+  position: fixed;
+}
 </style>
