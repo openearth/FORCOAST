b94e5a0ba097a55ee67e56fc01f2dc0bcd23a252
IoannaMi <ioannamicha@hotmail.com>
2021-04-28 19:58:02 +0200

    format and clean

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -4,19 +4,24 @@
       <h2 class="h2">Home</h2>
       <v-divider class="mt-4 mb-4" />
     </div>
-    <div v-html="content" />
+    <div v-html="content" class="introduction" />
   </div>
 </template>
 
 <script>
-import { importConfig } from '@/lib/config-utils'
+import { importConfig } from "@/lib/config-utils";
 
 export default {
-
   computed: {
     content() {
-      return importConfig(`content/introduction.md`)
-    }
-  }
+      return importConfig("content/introduction.md");
+    },
+  },
 };
 </script>
+<style>
+.v-application ul,
+.v-application ol {
+  margin-bottom: 16px;
+}
+</style>
