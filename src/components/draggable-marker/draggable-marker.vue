a6f5646e1fd488ffb67a0a2f5aa316385d878ae4
IoannaMi <ioannamicha@hotmail.com>
2021-04-28 19:57:30 +0200

    add draggable marker; draw polygon; emit event components

---
+++ src/components/draggable-marker/draggable-marker.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$emit("show-draggable-marker", true);
+  },
+  destroyed() {
+    this.$emit("show-draggable-marker", false);
+  },
+};
+</script>
---
+++ src/components/draw-polygon/draw-polygon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pa-4">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    this.$emit("show-draw-polygon", true);
+  },
+  destroyed() {
+    this.$emit("show-draw-polygon", false);
+  },
+};
+</script>
