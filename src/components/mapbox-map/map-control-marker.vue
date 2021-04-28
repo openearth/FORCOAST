68deccc7790e10af2d56b31ea83f6c7da09bb155
IoannaMi <ioannamicha@hotmail.com>
2021-04-28 20:01:17 +0200

    add mapbox control for marker and polygon draw

---
+++ src/components/mapbox-map/map-control-marker.vue
@@ -0,0 +1,51 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  inject: ["getMap"],
+  data() {
+    return {
+      marker: null,
+    };
+  },
+  props: {
+    center: {
+      type: Array,
+      required: true,
+    },
+  },
+  mounted() {
+    const map = this.getMap();
+    // if we are already loaded
+    if (map) {
+      this.addToMap(map);
+    }
+  },
+  destroyed() {
+    this.marker.remove();
+  },
+  methods: {
+    deferredMountedTo(map) {
+      if (this.center) {
+        this.addToMap(map);
+      }
+    },
+
+    addToMap(map) {
+      this.marker = new mapboxgl.Marker({
+        draggable: true,
+      })
+        .setLngLat([this.center[0], this.center[1]])
+        .addTo(map);
+
+      this.marker.on("dragend", () => {
+        const lngLat = this.marker.getLngLat();
+      });
+    },
+  },
+};
+</script>
