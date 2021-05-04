21b5330a4bf34195047753300bef83d8aad739cc
IoannaMi <ioannamicha@hotmail.com>
2021-05-04 06:52:25 +0200

    add min max allowed values; commit selected datespan

--- src/components/date-span/date-span.vue
+++ src/components/date-span/date-span.vue
@@ -13,33 +13,25 @@
         >
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
-                v-model="startDate"
-                label="Select a start date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
+              v-model="startDate"
+              label="Select a start date"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
             ></v-text-field>
           </template>
           <v-date-picker
-              v-model="startDate"
-              no-title
-              scrollable
+            v-model="startDate"
+            no-title
+            scrollable
+            min="2021-01-20"
+            max="2021-01-30"
           >
             <v-spacer></v-spacer>
-            <v-btn
-                text
-                color="primary"
-                @click="menu = false"
-            >
-                Cancel
-            </v-btn>
-            <v-btn
-                text
-                color="primary"
-                @click="$refs.menu.save(startDate)"
-            >
-                OK
+            <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+            <v-btn text color="primary" @click="$refs.menu.save(startDate)">
+              OK
             </v-btn>
           </v-date-picker>
         </v-menu>
@@ -56,33 +48,25 @@
         >
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
-                v-model="endDate"
-                label="Select an end date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
+              v-model="endDate"
+              label="Select an end date"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
             ></v-text-field>
           </template>
           <v-date-picker
-              v-model="endDate"
-              no-title
-              scrollable
+            v-model="endDate"
+            no-title
+            scrollable
+            min="2021-01-20"
+            max="2021-01-30"
           >
             <v-spacer></v-spacer>
-            <v-btn
-                text
-                color="primary"
-                @click="menu2 = false"
-            >
-                Cancel
-            </v-btn>
-            <v-btn
-                text
-                color="primary"
-                @click="$refs.menu2.save(endDate)"
-            >
-                OK
+            <v-btn text color="primary" @click="menu2 = false"> Cancel </v-btn>
+            <v-btn text color="primary" @click="$refs.menu2.save(endDate)">
+              OK
             </v-btn>
           </v-date-picker>
         </v-menu>
@@ -91,12 +75,35 @@
   </div>
 </template>
 <script>
+import createTimeSpan from "@/lib/create-time-span";
+import { parseISO, format } from "date-fns";
 export default {
-	data: () => ({
-		startDate: new Date().toISOString().substr(0, 10),
-		endDate: new Date().toISOString().substr(0, 10),
-		menu: false,
-		menu2: false,
-	}), 
-}
+  data: () => ({
+    startDate: "2021-01-20",
+    endDate: "2021-01-30",
+    menu: false,
+    menu2: false,
+    timeSpan: [],
+  }),
+  watch: {
+    startDate() {
+      this.timeSpan.push(this.startDate);
+      console.log("this.timespan", this.timeSpan);
+    },
+    endDate() {
+      this.timeSpan.push(this.endDate);
+      console.log("this.timespan end date", this.timeSpan);
+    },
+    timeSpan() {
+      if (this.timeSpan.length > 1) {
+        const start = new Date(this.startDate);
+        const end = new Date(this.endDate);
+        let days = createTimeSpan(start, end);
+
+        days = days.map((day) => day.toISOString().substr(0, 10));
+        this.$store.commit("SET_TIME_SPAN", days);
+      }
+    },
+  },
+};
 </script>
