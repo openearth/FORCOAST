a314da9222fa89fa99e4f38b45d8499597614b25
IoannaMi <ioannamicha@hotmail.com>
2021-05-27 15:33:12 +0200

    remove comment

--- src/lib/getWmsCapabilities/build-get-wms-capabilities.js
+++ src/lib/getWmsCapabilities/build-get-wms-capabilities.js
@@ -1,4 +1,2 @@
-export const describeCoverage = ({ url }) => 
-  `${url}?service=wms&version=1.1.1&request=GetCapabilities`;
-
-// http://forecoast.apps.k.terrasigna.com/geoserver/ows?service=wms&version=1.1.1&request=GetCapabilities&format=text/xml
+export const getCapabilities = ({ url }) => 
+  `${url}?service=WMS&version=1.1.1&request=GetCapabilities`;
