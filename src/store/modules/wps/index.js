f610224dc378fba55374315acd6aef7711015294
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-04-04 16:44:21 +0200

    Add files via upload

--- src/store/modules/wps/index.js
+++ src/store/modules/wps/index.js
@@ -12,7 +12,9 @@ export default {
     statusLink: null,
     selectedEntryValue: null,
     selectedEntryType: null,
-    calculationsTime: null,
+    selectedEntryValueOptional: null,
+    selectedEntryTypeOptional: null,
+    calculationsTime: null
 
   },
   mutations: {
@@ -34,11 +36,17 @@ export default {
     SET_SELECTED_ENTRY_VALUE(state, entryValue) {
       state.selectedEntryValue = entryValue
     },
+    SET_SELECTED_ENTRY_VALUE_OPTIONAL(state, entryValueOptional) {
+      state.selectedEntryValueOptional = entryValueOptional
+    },
     CLEAR_SELECTED_ENTRY_VALUE(state) {
       state.selectedEntryValue = null
     },
     SET_SELECTED_ENTRY_TYPE(state, entryType) {
       state.selectedEntryType = entryType
+    },
+    SET_SELECTED_ENTRY_TYPE_OPTIONAL(state, entryTypeOptional) {
+      state.selectedEntryTypeOptional = entryTypeOptional
     },
     SET_JOB_STATUS(state, status) {
       state.jobStatus = status
@@ -69,11 +77,17 @@ export default {
     setSelectedEntryValue(context, payload) {
       context.commit("SET_SELECTED_ENTRY_VALUE", payload)
     },
+    setSelectedEntryValueOptional(context, payload) {
+      context.commit("SET_SELECTED_ENTRY_VALUE_OPTIONAL", payload)
+    },
     clearSelectedEntryValue(context) {
       context.commit("CLEAR_SELECTED_ENTRY_VALUE")
     },
     setSelectedEntryType(context, payload) {
       context.commit("SET_SELECTED_ENTRY_TYPE", payload)
+    },
+    setSelectedEntryTypeOptional(context, payload) {
+      context.commit("SET_SELECTED_ENTRY_TYPE_OPTIONAL", payload)
     },
     clearJobStatus(context) {
       context.commit("CLEAR_JOB_STATUS")
@@ -85,7 +99,7 @@ export default {
     async runProcessor({commit, state, rootState}) {
 
       const {selectedAreaId, selectedService } = rootState.layers
-      const { markerLngLat, calculationsTime, selectedEntryType, selectedEntryValue, polygon, jobStatus } = state
+      const { markerLngLat, calculationsTime, selectedEntryType, selectedEntryValue, selectedEntryTypeOptional, selectedEntryValueOptional, polygon, jobStatus } = state
 
       // Set pilot area for selected service
       const area = selectedAreaId
@@ -93,10 +107,16 @@ export default {
       const id = selectedService.wps_id;
 
       // Set lat and lon input values
-      const { lat, lng }  = markerLngLat
+      var lat = ""
+      var lng = ""
+      if(markerLngLat) {
+        lat = markerLngLat.lat
+        lng = markerLngLat.lng
+      }
 
       // Set values from entry form
       //TODO move this into a function
+
       var lim = ""
       if (selectedEntryType == "lim") {
         lim = selectedEntryValue
@@ -105,6 +125,7 @@ export default {
       if (selectedEntryType == "period") {
         period = selectedEntryValue
       }
+      // Variables of A3
       var years = ""
       if (selectedEntryType[0] == "years"){
         years = selectedEntryValue[0]
@@ -134,31 +155,54 @@ export default {
         tu = selectedEntryValue[6]
       }
 
+     //Optional variables of A3
+      var kf = "0.75"
+      var o = "4.5"
+      var kr = "0.5"
+      var d = "-4"
+      
+      try {
+        if (selectedEntryTypeOptional[0] == "kf" && selectedEntryValueOptional[0] != ""){
+          kf = selectedEntryValueOptional[0]
+        } 
+        if (selectedEntryTypeOptional[1] == "o" && selectedEntryValueOptional[1] != ""){
+          o = selectedEntryValueOptional[1]
+        }
+        if (selectedEntryTypeOptional[2] == "kr" && selectedEntryValueOptional[2] != ""){
+          kr = selectedEntryValueOptional[2]
+        } 
+        if (selectedEntryTypeOptional[3] == "d" && selectedEntryValueOptional[3] != ""){
+          d = selectedEntryValueOptional[3]
+        } 
+      } catch {
+        //do nothing
+      }
+
       // Set source based on lat and lon
-      const source = "[" + markerLngLat.lng + "," + markerLngLat.lat + ", 0.5]"
+      var source = ""
+      if (markerLngLat) {
+      source = "[" + markerLngLat.lng + "," + markerLngLat.lat + ", 0.5]"
+      }
 
       // Set bounding box in proper format:
       // [[lon_min,lat_min],[lon_max,lat_min],[lon_max,lat_max],[lon_min,lat_max]]
       //TODO move this into a function
       var target = ""
       if (polygon) {
-        let lon_max = Math.max(polygon.features[0].geometry.coordinates[0][0][0], polygon.features[0].geometry.coordinates[0][1][0], polygon.features[0].geometry.coordinates[0][2][0], polygon.features[0].geometry.coordinates[0][3][0]);
-        let lon_min = Math.min(polygon.features[0].geometry.coordinates[0][0][0], polygon.features[0].geometry.coordinates[0][1][0], polygon.features[0].geometry.coordinates[0][2][0], polygon.features[0].geometry.coordinates[0][3][0]);
-        let lat_max = Math.max(polygon.features[0].geometry.coordinates[0][0][1], polygon.features[0].geometry.coordinates[0][1][1], polygon.features[0].geometry.coordinates[0][2][1], polygon.features[0].geometry.coordinates[0][3][1]);
-        let lat_min = Math.min(polygon.features[0].geometry.coordinates[0][0][1], polygon.features[0].geometry.coordinates[0][1][1], polygon.features[0].geometry.coordinates[0][2][1], polygon.features[0].geometry.coordinates[0][3][1]);
+        let lon_max = Math.max(polygon.features[0].geometry.coordinates[0][0][0], polygon.features[0].geometry.coordinates[0][1][0], polygon.features[0].geometry.coordinates[0][2][0], polygon.features[0].geometry.coordinates[0][3][0]).toFixed(10);
+        let lon_min = Math.min(polygon.features[0].geometry.coordinates[0][0][0], polygon.features[0].geometry.coordinates[0][1][0], polygon.features[0].geometry.coordinates[0][2][0], polygon.features[0].geometry.coordinates[0][3][0]).toFixed(10);
+        let lat_max = Math.max(polygon.features[0].geometry.coordinates[0][0][1], polygon.features[0].geometry.coordinates[0][1][1], polygon.features[0].geometry.coordinates[0][2][1], polygon.features[0].geometry.coordinates[0][3][1]).toFixed(10);
+        let lat_min = Math.min(polygon.features[0].geometry.coordinates[0][0][1], polygon.features[0].geometry.coordinates[0][1][1], polygon.features[0].geometry.coordinates[0][2][1], polygon.features[0].geometry.coordinates[0][3][1]).toFixed(10);
         // console.log(lon_max)
         // console.log(lon_min)
         // console.log(lat_max)
         // console.log(lat_min)
         target = "[[" + lon_min + "," + lat_min + "],[" + lon_max + "," + lat_min + "],[" + lon_max + "," + lat_max + "],[" + lon_min + "," + lat_max + "]]"
+      } else if (id == "a3") {
+        target = "[[8.1800000000,56.4500000000],[9.5000000000,56.4500000000],[9.5000000000,57.0500000000],[8.1800000000,57.0500000000]]"
       }
-      console.log(target)
 
-
-
-      console.log("years", years, "mb", mb, "me", me, "sl", sl, "su", su, "tl", tl, "tu", tu)
-
-      const response = await run(calculationsTime, period, id, area, source, target, lat, lng, lim, years, mb, me, sl, su, tl, tu)
+      const response = await run(calculationsTime, period, id, area, source, target, lat, lng, lim, years, mb, me, sl, su, tl, tu, kf, o, kr, d)
       //const response = await run(testtime, id)
       const href = response[0].value.href
 
