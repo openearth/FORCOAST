ceb073e9659c6e5f5cb83f87a4c9a53e6a99d26b
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-04-06 10:37:28 +0200

    Add files via upload

--- src/components/entry-form-a3/entry-form-a3.vue
+++ src/components/entry-form-a3/entry-form-a3.vue
@@ -3,63 +3,70 @@
     <v-container>
     <v-row>
       <v-col cols="4">
-        <v-subheader>{{ value_1 }}</v-subheader>
+        <v-subheader>Enter a year</v-subheader>
       </v-col>
       <v-col cols="8">
           <v-text-field
-            v-model="entryValue_1"
+            v-model="valueArray[0]"
             label="Year"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="4">
-        <v-subheader>{{ value_2 }}</v-subheader>
+        <v-subheader>Enter months</v-subheader>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_2"
+            v-model="valueArray[1]"
             label="Begin"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_3"
+            v-model="valueArray[2]"
             label="End"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="4">
-        <v-subheader>{{ value_4 }}</v-subheader>
+        <v-subheader>Salinity tresholds</v-subheader>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_4"
+            v-model="valueArray[3]"
             label="Lower"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_5"
+            v-model="valueArray[4]"
             label="Upper"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="4">
-        <v-subheader>{{ value_6 }}</v-subheader>
+        <v-subheader>Temperature tresholds</v-subheader>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_6"
+            v-model="valueArray[5]"
             label="Lower"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
       <v-col cols="4">
           <v-text-field
-            v-model="entryValue_7"
+            v-model="valueArray[6]"
             label="Upper"
+            v-on:input="entryValue"
           ></v-text-field>
       </v-col>
     </v-row>
@@ -68,94 +75,23 @@
 </template>
 <script>
 import { mapActions } from "vuex";
+import { mapState } from "vuex";
 export default {
   data: () => ({
-    entryValue_1: "",
-    entryValue_2: "",
-    entryValue_3: "",
-    entryValue_4: "",
-    entryValue_5: "",
-    entryValue_6: "",
-    entryValue_7: ""
+    valueArray: [],
+    //As string? or as numeric?, I did string for consistency
+    valueArrayOptional: ["0.75", "4.5", "0.5", "-4"]
   }),
-  props: {
-    value_1: {
-      type: String, 
-      required: true,
-    },
-    entryType_1: {
-      type: String, 
-      required: true,
-    },
-    value_2: {
-      type: String, 
-      required: true,
-    },
-    entryType_2: {
-      type: String, 
-      required: true,
-    },      
-    entryType_3: {
-      type: String, 
-      required: true,
-    },    
-    value_4: {
-      type: String, 
-      required: true,
-    },
-    entryType_4: {
-      type: String, 
-      required: true,
-    },
-    entryType_5: {
-      type: String, 
-      required: true,
-    },      
-    value_6: {
-      type: String, 
-      required: true,
-    },
-    entryType_6: {
-      type: String, 
-      required: true,
-    }, 
-    entryType_7: {
-      type: String, 
-      required: true,
-    },       
-  },
-  watch: {
-    entryValue_1() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_2() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_3() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_4() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_5() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_6() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    },
-    entryValue_7() {
-      this.setSelectedEntryType([this.entryType_1, this.entryType_2, this.entryType_3, this.entryType_4, this.entryType_5, this.entryType_6, this.entryType_7]);
-      this.setSelectedEntryValue([this.entryValue_1, this.entryValue_2, this.entryValue_3, this.entryValue_4, this.entryValue_5, this.entryValue_6, this.entryValue_7]);
-    }
-  },
   methods: { 
-    ...mapActions("wps", ["setSelectedEntryType", "setSelectedEntryValue"])
+    ...mapActions("wps", ["setSelectedEntryValue", "setSelectedEntryValueOptional"]),
+    ...mapState("wps", ["selectedEntryValueOptional"]),
+    entryValue() {
+      console.log(this.valueArray)
+      this.setSelectedEntryValue(this.valueArray)
+      if (this.selectedEntryValueOptional() == null) {
+        this.setSelectedEntryValueOptional(this.valueArrayOptional)
+      }
+    },
   }
 };
 </script>
