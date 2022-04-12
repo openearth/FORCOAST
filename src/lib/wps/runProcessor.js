4fe570c123caaeefbdad2ab56caa157b42cf59a6
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-04-12 17:10:02 +0200

    Add files via upload

--- src/lib/wps/runProcessor.js
+++ src/lib/wps/runProcessor.js
@@ -1,12 +1,24 @@
 import axios from "axios"
 import { template_f2 } from './template_f2'
+import { template_a4 } from './template_a4'
 import { template_a3 } from './template_a3'
 import { template_a2 } from './template_a2'
 import { template_a1 } from './template_a1'
 import { template_r1 } from './template_r1'
 
+// export default function(time, period, id, area, source, target, lat, lon, lim, years, mb, me, sl, su, tl, tu, kf, o, kr, d) {
 export default function(time, period, id, area, source, target, lat, lon, lim, selectedEntryValue, selectedEntryValueOptional) {
 	
+	// TODO: remove once done with debugging
+	console.log('runProcessor')
+	console.log(selectedEntryValue[0])
+	if (selectedEntryValueOptional == null){
+		console.log('Null!!')
+	} else {
+		console.log('Not null!')
+		console.log(selectedEntryValueOptional)
+	}
+	//
 	const data  = (() => {
 		if (id == "f2") {
 			return template_f2(time, id)
@@ -18,12 +30,14 @@ export default function(time, period, id, area, source, target, lat, lon, lim, s
 			return template_r1(area, time, period, lon, lat)
 		} else if (id == "a3") {
 			if (target == "") {
-				// default target for entire area, if no specific one is prescribed
 				target = "[[8.1800000000,56.4500000000],[9.5000000000,56.4500000000],[9.5000000000,57.0500000000],[8.1800000000,57.0500000000]]"
 			}
 			return template_a3(selectedEntryValue[0], selectedEntryValue[1], selectedEntryValue[2], selectedEntryValue[3], selectedEntryValue[4], selectedEntryValue[5], selectedEntryValue[6], selectedEntryValueOptional[0], selectedEntryValueOptional[1], selectedEntryValueOptional[2], selectedEntryValueOptional[3], target)
-		}
-
+							//(years, mb, me, sl, su, tl, tu, kf, o, kr, d, target)
+		} else if (id == "a4") {
+			return template_a4(selectedEntryValue[0], selectedEntryValue[1], selectedEntryValue[2], selectedEntryValue[3], selectedEntryValue[4], selectedEntryValue[5], selectedEntryValue[6])
+							//(ndays, ptm, tt, gdt, ttcs, pldmin, pldmax, sd)
+		}					
 	})();
 	return axios({
 		method: "post",
