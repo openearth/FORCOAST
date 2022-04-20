d1f66f139c4fa4051f7373371817991ad2d6cee1
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-04-20 15:10:56 +0200

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "Introduction" */ "../views/Introduction.vue"),
   },
   {
-    path: "/sector=:category&service=:service&area=:area",
+    path: "/:id",
     name: "Services",
     component: () => import(/* webpackChunkName: "Services" */ "../views/Services.vue"),
   },
