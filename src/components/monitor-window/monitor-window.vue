788e18bab5fd840cec022e722d2ce56d62ad73c3
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-06-29 17:11:24 +0200

    Now closes properly on outside click

--- src/components/monitor-window/monitor-window.vue
+++ src/components/monitor-window/monitor-window.vue
@@ -2,12 +2,16 @@
   <div>
     <v-dialog v-model="dialog" width="unset">
       <v-card height="unset" width="unset">
-        <div id="monitoring-container" ref="savecontent">
+        <div id="monitoring-container" ref="savecontent" v-on-clickaway="closeDialog">
           <v-img :src="imageTHREDDS"/>
           <v-img :src="imageGeoserver"/>
         </div>
         <v-card-actions class="justify-center">       
-          <v-btn color="primary" bottom text @click="closeDialog">
+          <v-btn color="primary" 
+                 bottom 
+                 text 
+                 @click="closeDialog"
+                 class="closeButton">
             Close
           </v-btn>    
         </v-card-actions>
@@ -17,6 +21,7 @@
 </template>
 <script>
 import featureInfo from "@/lib/featureInfo";
+import { mixin as clickaway } from 'vue-clickaway';
 
 export default {
   // see https://www.smashingmagazine.com/2020/01/data-components-vue-js/
@@ -27,6 +32,8 @@ export default {
       imageGeoserver: "https://minio.apps.k.terrasigna.com/forcoast/monitoring_bulletins/FORCOAST_geoserver_monitoring.png"
     };
   },
+  mixins: [ clickaway ],
+  template: '<p v-on-clickaway="away">Click away</p>',
   mounted() {
     var img = new Image();
     img.src = this.image;   
@@ -44,15 +51,20 @@ export default {
     },
     createGraph() {
       console.log("Graph!")
-    }
+    },
   },
 };
 </script>
-<style>
+<style scoped>
 #monitoring-container {
   width: unset;
   height: unset;
   margin-left: auto;
   margin-right: auto;
+}
+.closeButton {
+  font-size: 20px;
+  box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
 }
 </style>
