c02682091c5d5f5966d7b58d7cc4a3b0c803c558
danieltwigt <50665134+danieltwigt@users.noreply.github.com>
2022-07-11 10:57:32 +0200

    Make name of output file configurable (test, fix)
    Make name of output file configurable (test, fix)

--- src/components/status-card/status-card.vue
+++ src/components/status-card/status-card.vue
@@ -89,6 +89,7 @@ export default {
       this.jobId = this.statusLink.substring(this.statusLink.lastIndexOf('/') + 1);
       this.getStatusAttempt = this.getStatusAttempt + 1
       }
+      console.log('OUTPUTNAM STATUS-CARD', this.outputName)
     },
     cancel() {
       this.status = "cancelled"
