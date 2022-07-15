f3113107a4fdd0ac35bc197a0289969d5cfdbef8
GeoKonstant <105205646+GeoKonstant@users.noreply.github.com>
2022-07-15 12:15:01 +0200

    Add files via upload
    Added Bubble message to Data-viewer and Service-runnes. Increased the spacing of the main tabs and their width

--- src/components/collapsible-group/collapsible-group.vue
+++ src/components/collapsible-group/collapsible-group.vue
@@ -1,22 +1,33 @@
 <template>
   <v-expansion-panels v-model="open" :disabled="disabled" class="condensed">
+  <v-col>
     <v-expansion-panel>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+       
       <v-expansion-panel-header
         color="primary"
         ripple
         class="font-weight-bold px-4"
+        v-on="on"
       >
         {{ title }}
         <template v-if="checked" v-slot:actions>
           <v-icon>mdi-check-circle</v-icon>
         </template>
       </v-expansion-panel-header>
+      
+      </template>
+      <v-span>{{bubble}}</v-span>
+      </v-tooltip>
       <v-divider></v-divider>
+      
       <v-expansion-panel-content class="mx-n4 my-1">
         <div  v-if="manual" class="px-5 pb-3 flex-grow-1 overflow-y-auto" v-html="content" />
         <slot />
       </v-expansion-panel-content>
     </v-expansion-panel>
+    </v-col>
   </v-expansion-panels>
 </template>
 
@@ -27,6 +38,10 @@ import { importConfig } from "@/lib/config-utils"
 export default {
   props: {
     title: {
+      type: String,
+      required: false,
+    },
+    bubble: {
       type: String,
       required: false,
     },
@@ -62,23 +77,24 @@ export default {
         this.disabledButton = false;
       }
     },
-    selectedService() {
-      this.open = undefined
-    }
   },
 };
 </script>
 <style scoped>
 .v-expansion-panels.condensed .v-expansion-panel-header {
-  padding-top: 4px;
-  padding-bottom: 4px;
+  padding-top: 15px;
+  padding-bottom: 15px;
   min-height: auto;
+
+  
+
 }
 .v-expansion-panels.condensed
   .v-expansion-panel--active
   .v-expansion-panel-header {
-  padding-top: 8px;
-  padding-bottom: 8px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  
 }
 .v-expansion-panels.condensed .v-expansion-panel--active:not(:first-child),
 .v-expansion-panels.condensed .v-expansion-panel--active + .v-expansion-panel {
