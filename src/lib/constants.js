8aefbdf3de18c8eacc82da620e0aa449664301c2
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-07-27 17:41:27 +0200

    Change basemap layers

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 export const MAP_BASELAYERS = [
-	"light",
-	"satellite",
+	"mapbox://styles/forcoast/ckv2hfj623qkb14l9akxfvyzu",
+	"mapbox://styles/forcoast/cl61vdkzr000i14qzccif2k04",
 ].map(layer => ({
-	style: `mapbox://styles/mapbox/${ layer }-v9`,
+	style: `${ layer }`,
 	title: layer,
 	thumbnail: `/thumbnails/${ layer }.png`
 }))
