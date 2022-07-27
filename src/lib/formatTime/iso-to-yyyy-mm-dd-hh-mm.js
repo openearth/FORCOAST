03f3560b65dd6d75dc63ada6e1a1046dc2425229
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-07-27 17:40:53 +0200

    Change time formatting

--- src/lib/formatTime/iso-to-yyyy-mm-dd-hh-mm.js
+++ src/lib/formatTime/iso-to-yyyy-mm-dd-hh-mm.js
@@ -1,9 +1,10 @@
   import { format } from 'date-fns';
 
   export default((time) => {
+    const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     if (!time) {
       return null
     }
     
-    return format(new Date(time), 'yyyy-MM-dd HH:mm')
+    return weekday[new Date(time).getDay()] + format(new Date(time), ' yyyy-MM-dd HH:mm')
   })
---
+++ src/lib/formatTime/iso-to-yyyy-mm-dd.js
@@ -0,0 +1,10 @@
+import { format } from 'date-fns';
+
+export default((time) => {
+  const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  if (!time) {
+    return null
+  }
+  
+  return weekday[new Date(time).getDay()] + format(new Date(time), ' yyyy-MM-dd') 
+})
