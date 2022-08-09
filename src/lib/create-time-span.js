1de779484d2f571e213ba940c7bd363c00c9daf5
GidoStoop <84910774+GidoStoop@users.noreply.github.com>
2022-08-09 16:09:10 +0200

    If hourly data, create hour span

--- src/lib/create-time-span.js
+++ src/lib/create-time-span.js
@@ -1,8 +1,12 @@
-import { addDays, differenceInDays } from 'date-fns';
+import { addDays, addHours, differenceInDays, differenceInHours } from 'date-fns';
+import { mapState } from 'vuex';
 
-export default function (startDate, endDate) {
-    
-    const days = differenceInDays(endDate, startDate);
-    return [...Array(days+1).keys()].map((i) => addDays(startDate, i));
-  
+export default function (startDate, endDate, interval) {
+    if (interval == "hourly") {
+        const hours = differenceInHours(endDate, startDate)
+        return [...Array(hours+1).keys()].map((i) => addHours(startDate, i));
+    } else {
+        const days = differenceInDays(endDate, startDate);
+        return [...Array(days+1).keys()].map((i) => addDays(startDate, i));
+    }
 }
